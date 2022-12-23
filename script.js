957d16e2117b88cbe52bca699948564eacf12b01
Nicolas Fuentes <nfuentes11@gmail.com>
2022-12-23 10:50:31 -0500

    Console game finished.

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const gameArray = ['rock', 'paper', 'scissors' ];
-
+  let playerScore = 0;
+    let computerScore = 0;
 function game(){
   
-    for (let i = 0; i <= 3; i++) {
-        playRound(i);
+    for (let i = 0; i < 3; i++) {
+        playRound();
     }
-   
+    declareWinner(playerScore, computerScore);
 }
+
+function declareWinner(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+      console.log("You are the overall winner!");
+    } else if (computerScore > playerScore) {
+      console.log("The computer is the overall winner.");
+    } else {
+      console.log("It's a tie for the overall game!");
+    }
+  
+  }
+
 
 function playRound(){
     const playerSelection = playerChoice();
@@ -18,18 +31,25 @@ function playRound(){
                 } else if (computerSelection == 'rock' && playerSelection == 'scissors' ||
                              computerSelection == 'scissors' && playerSelection == 'paper' || 
                              computerSelection == 'paper' && playerSelection== 'rock') {
+                                 computerScore += 1
                      console.log('You lose')
                      
                  } else if (playerSelection == 'rock' && computerSelection == 'scissors' ||
                              playerSelection == 'scissors' && computerSelection == 'paper' || 
                              playerSelection == 'paper' && computerSelection == 'rock') {
+                                 playerScore += 1
                      console.log('You win')
                 }
+                
+
                 console.log(computerSelection);
                 console.log(playerSelection);
+
+               
+                
             }
 
-
+            
 function playerChoice(){
     let input= prompt('Let\'s play!!Pick rock, paper, or scissors. Type it in.');
         input = input.toLowerCase();
@@ -41,112 +61,7 @@ function playerChoice(){
 
 function getComputerChoice(){
     return gameArray[ Math.floor(Math.random() * gameArray.length)];
+
 }
 
 game();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//-------- check wiiner Fuenction ---------
-// function checkWinner(player, computer){
-//     if (player == computer){
-//         console.log('No one Wins!')
-       
-//        } else if (computer == 'Rock' && player== 'Scissors' ||
-//                     computer == 'Scissors' && player == 'Paper' || 
-//                     computer == 'Paper' && player== 'Rock') {
-//             console.log('You lose')
-            
-//         } else if (player == 'Rock' && computer == 'Scissors' ||
-//                     player == 'Scissors' && computer== 'Paper' || 
-//                     player == 'Paper' && computer == 'Rock') {
-//             console.log('You win')
-//        }
-
-// } this function was called to the playRound() fuctinton with a const
-
-
-
-
-
-
-// ------------- SECOND TRY-----------------
-// let playerSelection= prompt('Lets play!!Pick rock, paper, scissors. Type it in.');
-//     playerSelection = playerSelection.toLowerCase();
-// const computerSelection = gameArray[ Math.floor(Math.random() * gameArray.length)];
- 
-//     function playRound(playerSelection, computerSelection) {
-//         if (playerSelection == computerSelection){
-//          console.log('No one Wins!')
-        
-//         } else if (computerSelection == 'Rock' && playerSelection == 'Scissors' ||
-//                      computerSelection == 'Scissors' && playerSelection == 'Paper' || 
-//                      computerSelection == 'Paper' && playerSelection== 'Rock') {
-//              console.log('You lose')
-             
-//          } else if (playerSelection == 'Rock' && computerSelection == 'Scissors' ||
-//                      playerSelection == 'Scissors' && computerSelection == 'Paper' || 
-//                      playerSelection == 'Paper' && computerSelection == 'Rock') {
-//              console.log('You win')
-//         }
-        
-//          }
-         
-//          playRound(playerSelection, computerSelection);
-   
-    
-//    function game(){
-//     for (let i = 0; i < 3; i++) {
-//         playRound();
-//         let playerSelection= prompt('Lets play!!Pick rock, paper, scissors. Type it in.');
-//     playerSelection = playerSelection.toLowerCase();
-      
-//        }
-//    }
-//    game();
-   
-   
-   // FIRST CHOISE 
-
-   // function getComputerChoice (){
-//    let random = gameArray[ Math.floor(Math.random() * gameArray.length)];
-   
-// }
-//     getComputerChoice();
-       // function playRound(playerSelection, computerSelection) {
-    //    if ( playerSelection == 'Rock' && computerSelection == 'Scissors' ||
-    //         playerSelection == 'Scissors' && computerSelection == 'Paper' || 
-    //         playerSelection == 'Paper' && computerSelection == 'Rock'){
-
-    //    console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
-    //    }else{
-    //        console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
-    //    }
-    //     }
-        
-      
-    //   playRound();
-    //   console.log(playRound(playerSelection, computerSelection));
